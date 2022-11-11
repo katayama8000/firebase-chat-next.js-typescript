e989ce1d5b8108af610ce96415793d0a9c620de5
katayama <tattu.0310@gmail.com>
2022-11-11 14:42:06 +0900

    fix: type

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -16,3 +16,7 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore();
+
+export type User = {
+  uid: string | null | undefined;
+};
--- src/state/AuthContext.tsx
+++ src/state/AuthContext.tsx
@@ -1,17 +1,23 @@
+import type { User } from 'firebase/auth';
 import { onAuthStateChanged } from 'firebase/auth';
 import type { FC } from 'react';
 import { createContext, useEffect, useState } from 'react';
 
+//import type { User } from '../lib/firebase/firebase';
 import { auth } from '../lib/firebase/firebase';
 
 type Props = {
   children: React.ReactNode;
 };
 
-export const AuthContext = createContext();
+type AuthContextProps = {
+  currentUser: User | null | undefined;
+};
+
+export const AuthContext = createContext<AuthContextProps>({ currentUser: undefined });
 
 export const AuthContextProvider: FC<Props> = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState({});
+  const [currentUser, setCurrentUser] = useState<User | null | undefined>(undefined);
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
@@ -24,5 +30,5 @@ export const AuthContextProvider: FC<Props> = ({ children }) => {
     };
   }, []);
 
-  return <AuthContext.Provider value={{ currentUser }}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{ currentUser: currentUser }}>{children}</AuthContext.Provider>;
 };
